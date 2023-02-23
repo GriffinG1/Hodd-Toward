e8d553005531bbee836e9561fb929ad7c9360cb1
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2023-02-23 13:34:56 -0700

    Implement role toggling\nHardcoded for now

---
+++ cogs/roles.py
@@ -0,0 +1,77 @@
+import discord
+from discord.ext import commands
+
+
+class Roles(commands.Cog):
+    """Commands for managing roles."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.roles = {
+            # Pronouns
+            "they": 1078180553704091668,
+            "she": 1078180522624307310,
+            "he": 1078180484640669777,
+            "any": 1078180610939568218,
+
+            # Location
+            "uptown": 1078182681554194513,
+            "caphill": 1078182733743919114,
+            "cheesman": 1078182767373856778,
+            "other": 1078210712884748338
+        }
+        print(f"Loaded {self.__class__.__name__} cog.")
+
+    async def handle_roles(self, member, role, is_location=False):
+        """Handles role assignment."""
+        if role not in member.roles:
+            await member.add_roles(role)
+            if is_location:
+                return "✅ Added location role for {}.\n"
+            return "✅ Added pronoun role for `{}`.\n"
+        await member.remove_roles(role)
+        if is_location:
+            return "✅ Removed location role for {}.\n"
+        return "✅ Removed pronoun role for `{}`.\n"
+
+    @commands.command(aliases=["pronouns"])
+    async def set_pronouns(self, ctx, *, pronouns: str):
+        """Sets your pronouns. Use again to remove. Currently supported pronouns: they, she, he, any. Please ask a mod if yours aren't available.
+        Please note that combined pronouns (ex she/they) are not supported, please separate them. (ex she | they)
+        Example: .set_pronouns they | she > adds the pronoun roles for they/them and she/her"""
+        pronouns = pronouns.lower().replace(" ", "").split("|")
+        message = ""
+        invalid_pronouns = []
+        for pronoun in pronouns:
+            if pronoun == "they" or pronoun == "them" or pronoun == "they/them":
+                message += (await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["they"]))).format("they/them")
+            elif pronoun == "she" or pronoun == "her" or pronoun == "she/her":
+                message += (await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["she"]))).format("she/her")
+            elif pronoun == "he" or pronoun == "him" or pronoun == "he/him":
+                message += (await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["he"]))).format("he/him")
+            elif pronoun == "any":
+                message += (await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["any"]))).format("any")
+            else:
+                invalid_pronouns.append(pronoun)
+        if invalid_pronouns:
+            message += f"❌ Some pronouns were not recognized. Please ask a mod if yours aren't available.\nUnrecognized pronouns: `{'`, `'.join(invalid_pronouns)}`"
+        await ctx.send(message)
+
+    @commands.command(aliases=["location"])
+    async def set_location(self, ctx, *, location: str):
+        """Set your location in Cap Hill. Use again to remove. Currently supported locations: Uptown, Capitol Hill, Cheesman Park, Other. See #resources for more info."""
+        if location.lower() == "uptown":
+            message = await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["uptown"]), True)
+            return await ctx.send(message.format("Uptown"))
+        elif location.lower() == "caphill" or location.lower() == "capitol hill" or location.lower() == "cap hill":
+            message = await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["caphill"]), True)
+            return await ctx.send(message.format("Capitol Hill"))
+        elif location.lower() == "cheesman" or location == "cheesman park":
+            message = await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["cheesman"]), True)
+            return await ctx.send(message.format("Cheesman Park"))
+        message = await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["other"]), True)
+        return await ctx.send(message.format("Other"))
+
+
+async def setup(bot):
+    await bot.add_cog(Roles(bot))
