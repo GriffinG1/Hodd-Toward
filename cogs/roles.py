4d5c490f2a328c2aed2c03de5143c9bbfff61d57
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2023-03-08 18:50:59 -0700

    Add listpronouns command, fix handle_roles

--- cogs/roles.py
+++ cogs/roles.py
@@ -16,30 +16,30 @@ class Roles(commands.Cog):
             self.roles = json.load(file)
         print(f"Loaded {self.__class__.__name__} cog.")
 
-    async def handle_roles(self, member, role, is_location=False):
+    async def handle_roles(self, member, role_name):
         """Handles role assignment."""
+        if role_name not in self.roles["pronouns"].keys():
+            return False
+        role = discord.utils.get(member.guild.roles, id=self.roles["pronouns"][role_name])
         if role not in member.roles:
             await member.add_roles(role)
-            if is_location:
-                return "✅ Added location role for {}.\n"
             return "✅ Added pronoun role for `{}`.\n"
         await member.remove_roles(role)
-        if is_location:
-            return "✅ Removed location role for {}.\n"
         return "✅ Removed pronoun role for `{}`.\n"
 
     @commands.command(aliases=["pronouns"])
     async def set_pronouns(self, ctx, *, pronouns: str):
         """Sets your pronouns. Use again to remove.
-        Please note that combined pronouns (ex she/they) are not supported, please separate them. (ex she | they)
+        Please note that combined pronouns (ex she/they) are not supported, please separate them. (ex she | they).
+        You can use ?lp for a list of pronouns.
         Please ask for help if your pronouns aren't available.
 
-        Example: .set_pronouns they | she > adds the pronoun roles for they/them and she/her"""
+        Example: ?set_pronouns they | she > adds the pronoun roles for they/them and she/her"""
         pronouns = pronouns.lower().replace(" ", "").split("|")
         message = ""
         invalid_pronouns = []
         for pronoun in pronouns:
-            resp = await self.handle_roles(ctx.author, ctx.guild.get_role(self.roles["they"]))
+            resp = await self.handle_roles(ctx.author, pronoun)
             if not resp:
                 invalid_pronouns.append(pronoun)
                 continue
@@ -53,40 +53,46 @@ class Roles(commands.Cog):
     @commands.command(aliases=["addpronouns"])
     async def add_pronoun(self, ctx, pronoun: str, role: discord.Role):
         """Adds a pronoun role to the list of available roles.
-        Example: .add_pronoun they @they/them"""
+        Example: ?add_pronoun they @they/them"""
         pronoun = pronoun.lower().replace(" ", "")
-        if pronoun in self.roles.keys():
+        if pronoun in self.roles["pronouns"].keys():
             return await ctx.send(f"❌ Pronoun `{pronoun}` is already in the list. Please use `.edit_pronoun` to edit it.")
-        self.roles[pronoun] = role.id
+        self.roles["pronouns"][pronoun] = role.id
         with open("data/roles.json", "w") as file:
             json.dump(self.roles, file, indent=4)
         await ctx.send(f"✅ Added pronoun role for `{pronoun}`.")
 
     @commands.has_permissions(manage_roles=True)
-    @commands.command(aliaes=["editpronouns"])
+    @commands.command(aliases=["editpronouns"])
     async def edit_pronoun(self, ctx, pronoun: str, new_role: discord.Role):
         """Edits a pronoun role in the list of available roles.
-        Example: .edit_pronoun they @they/them"""
+        Example: ?edit_pronoun they @they/them"""
         pronoun = pronoun.lower().replace(" ", "")
         if pronoun not in self.roles.keys():
             return await ctx.send(f"❌ Pronoun `{pronoun}` is not in the list. Please use `.add_pronoun` to add it.")
-        self.roles[pronoun] = new_role.id
+        self.roles["pronouns"][pronoun] = new_role.id
         with open("data/roles.json", "w") as file:
             json.dump(self.roles, file, indent=4)
         await ctx.send(f"✅ Edited pronoun role for `{pronoun}`.")
 
     @commands.has_permissions(manage_roles=True)
-    @commands.command(aliaes=["removepronouns"])
+    @commands.command(aliases=["removepronouns"])
     async def remove_pronoun(self, ctx, pronoun: str):
         """Removes a pronoun role from the list of available roles.
-        Example: .remove_pronoun they"""
+        Example: ?remove_pronoun they"""
         pronoun = pronoun.lower().replace(" ", "")
-        if pronoun not in self.roles.keys():
+        if pronoun not in self.roles["pronouns"].keys():
             return await ctx.send(f"❌ Pronoun `{pronoun}` is not in the list.")
-        self.roles.pop(pronoun)
+        self.roles["pronouns"].pop(pronoun)
         with open("data/roles.json", "w") as file:
             json.dump(self.roles, file, indent=4)
         await ctx.send(f"✅ Removed pronoun role for `{pronoun}`.")
+
+    @commands.command(aliases=["listpronouns", "lp"])
+    async def list_pronouns(self, ctx):
+        """Lists all available pronoun roles."""
+        pronouns = sorted([f"`{pronoun.title()}`" for pronoun in self.roles["pronouns"].keys()])
+        await ctx.send(f"Available pronouns: {', '.join(pronouns)}")
 
 
 async def setup(bot):
